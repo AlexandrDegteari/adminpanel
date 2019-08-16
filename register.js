9d843d08c13aab8aab131a471e41b04347720207
Paul Talmaci <paul.talmaci@gmail.com>
2019-08-16 16:54:01 +0300

    modify register

--- register.js
+++ register.js
@@ -1,16 +1,17 @@
 const form = document.querySelector('form');
-const input = document.getElementById("exampleInputEmail");
+const input = document.getElementById("exampleFirstName");
+const input2 = document.getElementById("exampleLastName");
+const input3 = document.getElementById("exampleInputEmail");
+const input4 = document.getElementById("exampleInputPassword");
+let ids = localStorage.getItem('id') ? localStorage.getItem('id') : 0;
+
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
 localStorage.setItem('items', JSON.stringify(itemsArray));
-
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    itemsArray.push(input.value);
+    ids++;
+    localStorage.setItem('id', JSON.stringify(ids));
+    itemsArray.push({username:input.value,User2:input2.value , email:input3 ,password:input4, id : ids});
     localStorage.setItem('items', JSON.stringify(itemsArray));
 });
-
-
-
-
