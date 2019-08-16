db6f1ddb92266458e5beebb7b1c21773543cf3c3
Paul Talmaci <paul.talmaci@gmail.com>
2019-08-16 12:18:44 +0300

    add login and register js

---
+++ login.js
@@ -0,0 +1,16 @@
+// const form = document.querySelector('form');
+// const input = document.getElementById("exampleInputEmail");
+// let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+//
+// localStorage.setItem('items', JSON.stringify(itemsArray));
+//
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//
+//     itemsArray.push(input.value);
+//     localStorage.setItem('items', JSON.stringify(itemsArray));
+// });
+//
+//
+
+
