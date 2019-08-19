28f24274f292eb9256a0ecf759d448b89fa0a641
Alex <1989alex1989@gmail.com>
2019-08-19 13:43:22 +0300

    fixed login2

--- login.js
+++ login.js
@@ -1,48 +1,8 @@
-/*
-const form = document.querySelector('form');
-const input = document.getElementById("exampleInputEmail");
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-
-localStorage.setItem('items', JSON.stringify(itemsArray));
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    itemsArray.push(input.value);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-});
-
-
-
-*/
-
 let loginMail = document.getElementById("exampleInputEmail"),
     loginPass = document.getElementById("exampleInputPassword"),
     loginBtn = document.getElementById("loginBtn"),
     loginGoogleBtn = document.getElementById("loginGoogleBtn"),
     loginFbBtn = document.getElementById("loginFbBtn");
-// let userObject = [
-//     {
-//         userMail: "alex@mail.ru",
-//         userPass: 12345
-//     },
-//     {
-//         userMail: "vlad@mail.ru",
-//         userPass: 54321
-//     },
-//     {
-//         userMail: "vasea@mail.ru",
-//         userPass: 11111
-//     },
-//     {
-//         userMail: "fedea@mail.ru",
-//         userPass: 22222
-//     },
-//     {
-//         userMail: "jora@mail.ru",
-//         userPass: 33333
-//     },
-// ];
 loginBtn.addEventListener("click", getInfo);
 
 function getInfo(e) {
@@ -51,6 +11,7 @@ function getInfo(e) {
     for (let i = 0; i < itemsArray.length; i++) {
         if (loginMail.value == itemsArray[i].email && loginPass.value == itemsArray[i].password) {
             console.log(loginMail.value + " logged in!!!");
+
             location.href = "index.html";
             return
         }
@@ -61,42 +22,5 @@ function getInfo(e) {
 
 }
 
-// let userObject = [
-//     {
-//         userMail: "alex@mail.ru",
-//         userPass: 12345
-//     },
-//     {
-//         userMail: "vlad@mail.ru",
-//         userPass: 54321
-//     },
-//     {
-//         userMail: "vasea@mail.ru",
-//         userPass: 11111
-//     },
-//     {
-//         userMail: "fedea@mail.ru",
-//         userPass: 22222
-//     },
-//     {
-//         userMail: "jora@mail.ru",
-//         userPass: 33333
-//     },
-// ];
-// loginBtn.addEventListener("click", getInfo);
-//
-// function getInfo(e) {
-//     e.preventDefault();
-//     console.log("work");
-//     for (let i = 0; i < userObject.length; i++) {
-//         if (loginMail.value == userObject[i].userMail && loginPass.value == userObject[i].userPass) {
-//             console.log(loginMail.value + " logged in!!!");
-//             return
-//         }
-//         console.log("Username or password incorrect")
-//     }
-//
-// }
-
-
-
+var logUser = loginMail.value;
+export default logUser;
